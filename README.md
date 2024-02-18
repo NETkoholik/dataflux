a77518bc39d11346c374ef993d53e2a09c78c6ad
NETkoholik <NETkoholik+github@gmail.com>
2024-02-18 19:25:58 -0300

    Merged all three repos into one

---
+++ README.md
@@ -0,0 +1,3 @@
+# 1 pound symbol. This is a test system, part of the dataflux series
+## 2 pound symbols
+No pound symbols here
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+	<meta lang="es">
+	<title>dataflux resos - Iniciar sesi&oacute;n</title>
+	<link rel="stylesheet" href="login.css">
+</head>
+<body>
+	<form method="post" action="">
+		<h2>Iniciar sesi&oacute;n</h2>
+		<label for="username">Nombre de usuario</label>
+		<input type="text" id="username" name="username" required>
+		<label for="password">Contrase&ntilde;a </label>
+		<input type="password" id="password" name="password" required>
+		<input type="submit" value="Iniciar sesi&oacute;n"><br>
+        <br>&iquest;Olvidaste tu contrase&ntilde;a? <a href="login.php">Restabl&eacute;cela.</a>
+		<br>&iquest;No tienes usuario? <a href="login.php">Cr&eacute;ate uno.</a>
+	</form>
+</body>
+</html>
+
